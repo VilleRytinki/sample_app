acbe792412fec3ceff5ebe965f4dfad525acb9cf
Ville Rytinki <vrytinki@gmail.com>
2025-11-06 11:04:57 +0200

    included npm and yarn in build file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,15 @@
 
 set -o errexit
 
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
+apt-get install -y nodejs
+npm install -g yarn
+
 bundle install
+
+yarn install --frozen-lockfile
+yarn build:css
+
 bin/rails assets:precompile
 bin/rails assets:clean
 
