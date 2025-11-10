99da9245e9b97c4eea40dc2e6eae4bc03b1c5870
Ville Rytinki <vrytinki@gmail.com>
2025-11-10 14:06:58 +0200

    changed implementation to use case_sensitive uniqueness but to downcase emails before validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: URI::MailTo::EMAIL_REGEXP },
-            uniqueness: { case_sensitive: false }
+            uniqueness: { case_sensitive: true }
 end
