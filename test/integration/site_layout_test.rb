7af1b3179fa08fa6365b42c4e660e92f4ee7b796
Ville Rytinki <vrytinki@gmail.com>
2025-11-10 09:17:35 +0200

    created tests for layout file links

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,27 @@
+require "test_helper"
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    get root_path
+  end
+
+  test "root path renders home template" do
+    assert_template "static_pages/home"
+  end
+
+  test "layout file has 2 root navigation links" do
+    assert_select "a[href=?]", root_path, count: 2
+  end
+
+  test "layout file has link to help page" do
+    assert_select "a[href=?]", help_path
+  end
+
+  test "layout file has link to about page" do
+    assert_select "a[href=?]", about_path
+  end
+
+  test "layout file has link to contact page" do
+    assert_select "a[href=?]", contact_path
+  end
+end
