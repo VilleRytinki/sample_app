d3be49c907d8425e9f52d6fb3f335428b2562b01
Ville Rytinki <vrytinki@gmail.com>
2025-11-12 11:17:58 +0200

    replaced mass assigment with strong parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
 
     if @user.save
       # handle successful save.
     else
       render "new", status: unprosessable_entity
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
