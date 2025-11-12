1f391f1a54d1d4c07848436dab0adb6557e802c2
Ville Rytinki <vrytinki@gmail.com>
2025-11-12 14:48:47 +0200

    created a test for succesful user creation and implemented redirect on creating new user.

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -17,4 +17,30 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template "users/new"
     assert_select "li.error-message", 3
   end
+
+  test "valid signup information" do
+    before = User.count
+
+    post users_path, params: {
+      user: {
+        name: "valid_user",
+        email: "valid@validuser.com",
+        password: "testuser123",
+        password_confirmation: "testuser123"
+      }
+    }
+
+    after = User.count
+    user = assigns(:user)
+
+      message = if after == before && user
+              "Expected User.count to increase by 1, but it didn’t.\n" \
+              "Validation errors: #{user.errors.full_messages.join(', ')}"
+      else
+              "Expected User.count to increase by 1."
+      end
+
+  assert_equal before + 1, after, message
+  assert_redirected_to user_path(User.last)
+end
 end
