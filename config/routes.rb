c61d8f7b1199768558484dfdb189dd152b899ac0
Ville Rytinki <vrytinki@gmail.com>
2025-11-13 12:56:39 +0200

    updated routes for session state

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get "sessions/new"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/login", to: "sessions#destroy"
   root "static_pages#home"
   get "/home",  to: "static_pages#home"
   get "/help",  to: "static_pages#help"
