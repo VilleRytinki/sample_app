9e1dae2b85878c60d06e739e9c3fe86827ad4a51
Ville Rytinki <vrytinki@gmail.com>
2025-11-14 11:22:42 +0200

    implemented functionality for user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to profile page
+      reset_session
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = "Invalid email/password combination"
       render "new", status: :unprocessable_entity
