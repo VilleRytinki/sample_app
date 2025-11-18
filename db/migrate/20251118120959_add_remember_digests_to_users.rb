87e900226056c7c5bc4259d185d7480246bbfdec
Ville Rytinki <vrytinki@gmail.com>
2025-11-18 14:19:08 +0200

    migrations for remember token digest

---
+++ db/migrate/20251118120959_add_remember_digests_to_users.rb
@@ -0,0 +1,5 @@
+class AddRememberDigestsToUsers < ActiveRecord::Migration[8.1]
+  def change
+    add_column :users, :remember_digest, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_11_070115) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_18_120959) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_11_070115) do
     t.string "email"
     t.string "name"
     t.string "password_digest"
+    t.string "remember_digest"
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
