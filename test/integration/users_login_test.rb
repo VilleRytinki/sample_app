518fd9239da43a13e3aee088662146f38580add4
Ville Rytinki <66476143+VilleRytinki@users.noreply.github.com>
2025-11-19 15:27:49 +0200

    Advanced login (#10)
    * migrations for remember token digest
    
    * added remember token to enable persistent sessions
    
    * added remember method to session to remember users by default
    
    * modified current_user to use cookies functionality
    
    * added functionality to delete persistent cookies on logout
    
    * fixed a bug where user logout resulted in error if logging out on a secondary window after logging out on the first.
    
    * fixed a bug where authenticated? method results in error because current user evaluates cookies to true and proceed to evaluate nil object.
    
    * front end for checkbox
    
    * implemented remember me functionality on session create ie. user login
    
    * implemented tests for remember_me cookie
    
    * added test for session_helpers current users remember path
    
    * added session token to prevent session replay attacks.
    
    * updated Readme for chapter 9

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -85,4 +85,21 @@ class LogoutTest < Logout
     assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
+
+  test "should still work after logout in second window" do
+    delete logout_path
+    assert_redirected_to root_url
+  end
+end
+
+class RememberingTest < UsersLogin
+  test "login with remembering" do
+    log_in_with(user_email: @user.email, password: ActiveSupport::TestCase::FIXTURE_PASSWORD, remember_me: "1")
+    assert_not cookies[:remember_token].blank?
+  end
+
+  test "login without remembering" do
+    log_in_with(user_email: @user.email, password: ActiveSupport::TestCase::FIXTURE_PASSWORD, remember_me: "0")
+    assert cookies[:remember_token].blank?
+  end
 end
