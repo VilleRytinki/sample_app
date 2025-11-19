ef96732bf3a7ad62624c42fedc5b8c22bd37319b
Ville Rytinki <vrytinki@gmail.com>
2025-11-19 15:11:57 +0200

    added session token to prevent session replay attacks.

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,9 +18,14 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
 
+  def session_token
+    remember_digest || remember
+  end
+
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+    remember_digest
   end
 
   def authenticated?(remember_token)
