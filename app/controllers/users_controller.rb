091d961bd8f3bd88a054025fc78a2fa5111c058f
Ville Rytinki <66476143+VilleRytinki@users.noreply.github.com>
2025-11-25 11:25:17 +0200

    User edit authorization (#11)
    * required logged in for edit and update actions. Updated tests.
    
    * limiting edit and update actions to current user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [ :edit, :update ]
+  before_action :correct_user, only: [ :edit, :update ]
   def new
     @user = User.new
   end
@@ -37,5 +39,18 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url, status: :see_other
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      user_not_nil_and_current_user = @user&. == current_user
+      redirect_to(root_url, status: :see_other) unless user_not_nil_and_current_user
     end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,50 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:testuser1)
+    @user2 = users(:testuser2)
+  end
   test "should get signup" do
     get signup_path
     assert_response :success
     assert_select "title", full_title("Sign Up")
+  end
+
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_redirected_to login_path
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: {
+      user: {
+        name: @user.name,
+        email: @user.email
+      }
+    }
+    assert_not flash.empty?
+    assert_redirected_to login_path
+  end
+
+  test "should redirect edit when logged in as a wrong user" do
+    log_in_with(user_email: @user.email, password: FIXTURE_PASSWORD)
+    get edit_user_path(@user2)
+
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as a wrong user" do
+    log_in_with(user_email: @user.email, password: FIXTURE_PASSWORD)
+    patch user_path(@user2), params: {
+      user: {
+        name: @user2.name,
+        email: @user2.email
+      }
+    }
+
+    assert flash.empty?
+    assert_redirected_to root_url
   end
 end
